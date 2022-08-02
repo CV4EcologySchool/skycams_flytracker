2ecd2bc1257a3ae2a15ec3200221027942f3ad1a
fponce <fponce@caltech.edu>
2022-08-02 15:20:13 -0700

    added a test plot script2

--- plot_labeled_data_info.py
+++ plot_labeled_data_info.py
@@ -4,13 +4,14 @@ import numpy as np
 import os
 import time
 from subprocess import call
+import cv2
 ###############################################################################
 
 # input raw video directory
-DataDir = '/Volumes/COMPA/upward_facing_cameras_data/raw_data'
+#DataDir = '/Volumes/COMPA/upward_facing_cameras_data/raw_data'
 
-image_path = ''
+image_path = '/home/ubuntu/work/labeled_data/0419_084547/0419-upcam_01_vi_0001_20190419_084547_trimmed-34614.jpg'
 
-example_frame = Image.open(BytesIO(image_path))
-print(example_frame.size)
-#example_frame
+img = cv2.imread(image_path)
+# Output img with window name as 'image'
+cv2.imshow('image', img)
