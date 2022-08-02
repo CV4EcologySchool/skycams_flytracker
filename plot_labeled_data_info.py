dfaaad63b5e021b3399f24584c4df933e9c0f0d7
fponce <fponce@caltech.edu>
2022-08-02 13:24:18 -0700

    added a test plot script

---
+++ plot_labeled_data_info.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import warnings
+import numpy as np
+import os
+import time
+from subprocess import call
+###############################################################################
+
+# input raw video directory
+DataDir = '/Volumes/COMPA/upward_facing_cameras_data/raw_data'
+
+image_path = ''
+
+example_frame = Image.open(BytesIO(image_path))
+print(example_frame.size)
+#example_frame
