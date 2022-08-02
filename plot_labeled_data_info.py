1d9e0790f04b006be33ba111ac50b10f771d7697
fponce <fponce@caltech.edu>
2022-08-02 15:28:48 -0700

    added a test plot script3

--- plot_labeled_data_info.py
+++ plot_labeled_data_info.py
@@ -5,13 +5,24 @@ import os
 import time
 from subprocess import call
 import cv2
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 ###############################################################################
 
 # input raw video directory
 #DataDir = '/Volumes/COMPA/upward_facing_cameras_data/raw_data'
 
-image_path = '/home/ubuntu/work/labeled_data/0419_084547/0419-upcam_01_vi_0001_20190419_084547_trimmed-34614.jpg'
+# image_path = '/home/ubuntu/work/labeled_data/0419_084547/0419-upcam_01_vi_0001_20190419_084547_trimmed-34614.jpg'
+#
+# img = cv2.imread(image_path)
+# # Output img with window name as 'image'
+# cv2.imshow('image', img)
 
-img = cv2.imread(image_path)
-# Output img with window name as 'image'
-cv2.imshow('image', img)
+# Python program to read
+# image using matplotlib
+
+# Read Images
+img = mpimg.imread('g4g.png')
+
+# Output Images
+plt.imshow(img)
